c66c41620354316f2a1b457cb69bf66d4eef96fc
vktechturbine <vkerlekar@techturbine.com>
2023-06-24 12:39:09 +0530

    Add New changes

---
+++ build/precache-manifest.c6990c607047747f8005d784a64371c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a0a762128cc169ab187",
+    "url": "/static/css/main.60dc3d3d.chunk.css"
+  },
+  {
+    "revision": "6a0a762128cc169ab187",
+    "url": "/static/js/main.6a0a7621.chunk.js"
+  },
+  {
+    "revision": "26603eca02e78761fa9e",
+    "url": "/static/js/1.26603eca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a22a164a397813eb31cdfc92d0779882",
+    "url": "/index.html"
+  }
+];
